c528316dec78b3e7806e02fd2c0f0dbdf0f02429
Frank Bardon Jr <nerdsrescueme@gmail.com>
2012-01-09 12:41:28 -0500

    Page: Formatted page model code

--- js/models/pages.js
+++ js/models/pages.js
@@ -1,10 +1,18 @@
-define(['underscore', 'backbone'], function(_, Backbone) {
-	var pagesModel = Backbone.Model.extend({
+define([
+	'underscore', 
+	'backbone'
+], 
+function(_, Backbone) {
+
+	var PageModel = Backbone.Model.extend({
 		defaults: {
-			title: 'Untitled',
+			title: 'Untitled Page',
+			published: false,
 			score: 10
 		},
 		initialize: function() {}
 	});
-	return pagesModel;
+	return PageModel;
+
+
 });
